b865260fd3148c4dc6a31bbac29ce184679e4c15
jonas05PJ <114864472+jonas05PJ@users.noreply.github.com>
2024-04-18 13:15:15 +0100

    Update main.tf

--- try1/main.tf
+++ try1/main.tf
@@ -35,6 +35,31 @@ content =tls_private_key.rsa_4096.private_key_pem
 filename = var.key_name
 
 }
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "hackathon_instance" {
   ami           = "ami-00c71bd4d220aa22a"
@@ -45,3 +70,48 @@ resource "aws_instance" "hackathon_instance" {
     Name = "terrraform_instance"
   }
 }
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt upgrade -y",
+      "sudo apt install apache2 -y",
+      "sudo systemctl start apache2",
+      "sudo systemctl enable apache2",
+      "sudo apt install mariadb-server mariadb-client -y",
+      "sudo systemctl start mariadb",
+      "sudo systemctl enable mariadb",
+      "sudo mysql-secure-installation",
+       "sudo systemctl restart mariadb",
+       "sudo apt install php-mysql php-gd php-cli php-common -y",
+ 
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt install wget -y",
+      "sudo wget  https://wordpress.org/latest.zip",
+      "sudo unzip latest.zip",
+      "sudo cp -r wordpress/* /var/www/html/",
+      "cd /var/www/html/",
+      "sudo chown -R www-data:www-data /var/www/html/",
+      "sudo rm -rf index.html",
+      "cd",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mysql -u root -p \"CREATE DATABASE wordpress;\"",
+      "sudo mysql -u root -p \"CREATE USER 'wpadmin' IDENTIFIED BY 'wpadminpass';\"",
+      "sudo mysql -u root -p \"GRANT ALL PRIVILEGES ON wordpress.* TO 'wpadmin';\"",
+      "sudo mysql -u root -p \"FLUSH PRIVILEGES;\"",
+    ]
+  }
+}
+
